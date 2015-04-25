039dd9b86a7e94c01f49032ae84454ef60068849
Pandian <pandian.raju@flipkart.com>
2015-04-25 16:01:03 +0530

    Adding dummy classes

---
+++ src/main/java/com/housing/recoland/db/TestDB/Java.java
@@ -0,0 +1,7 @@
+package com.housing.recoland.db.TestDB;
+
+/**
+ * Created by pandian.raju on 25/04/15.
+ */
+public class Java {
+}
---
+++ src/main/java/com/housing/recoland/service/MainService/java.java
@@ -0,0 +1,7 @@
+package com.housing.recoland.service.MainService;
+
+/**
+ * Created by pandian.raju on 25/04/15.
+ */
+public class java {
+}
