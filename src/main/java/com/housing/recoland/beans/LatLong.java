403a49ee2d9a4b8b4795fb794328723e6d076201
Pandian <pandian.raju@flipkart.com>
2015-04-25 19:06:46 +0530

    Adding constructors for LatLong

--- src/main/java/com/housing/recoland/beans/LatLong.java
+++ src/main/java/com/housing/recoland/beans/LatLong.java
@@ -6,4 +6,12 @@ package com.housing.recoland.beans;
 public class LatLong {
     public Double latitude;
     public Double longitude;
+
+    public LatLong() {
+    }
+
+    public LatLong(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
