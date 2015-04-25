c6f3fa8bfa681af7fe32ccae675d8b2a6d36ab5b
Pandian <pandian.raju@flipkart.com>
2015-04-25 18:44:21 +0530

    Adding getTheaterRating function

--- src/main/java/com/housing/recoland/service/utils/RatingCalculator.java
+++ src/main/java/com/housing/recoland/service/utils/RatingCalculator.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import com.housing.recoland.db.models.HotelDetails;
 import com.housing.recoland.db.models.LandDetails;
+import com.housing.recoland.db.models.TheaterDetails;
 
 public class RatingCalculator {
 
@@ -19,8 +20,8 @@ public class RatingCalculator {
     }
 
     public static double getHotelRating(List<HotelDetails> hotelList, Set<String> cuisineSet, LandDetails land){
-        double score=0;
-        int hotelCount=0;
+        double score = 0;
+        int hotelCount = 0;
         for(HotelDetails hotel : hotelList){
             if(cuisineSet.contains(hotel.getCuisineType())){
                 score += (hotel.getRating() * 10 + getDistanceScore(DistanceCalculator.getDistance(hotel.getLatitude(),
@@ -29,6 +30,20 @@ public class RatingCalculator {
             }
         }
         double averageScore = score / hotelCount;
+        return (averageScore / 100) * 3;
+    }
+
+    public static double getTheaterRating(List<TheaterDetails> theaterList, Set<String> movieTypeSet, LandDetails land){
+        double score = 0;
+        int theaterCount = 0;
+        for(TheaterDetails theater : theaterList){
+            if(movieTypeSet.contains(theater.getMovieType())){
+                score += (theater.getRating() * 10 + getDistanceScore(DistanceCalculator.getDistance(theater.getLatitude(),
+                        theater.getLongitude(), land.getLatitude(), land.getLongitude())));
+                theaterCount++;
+            }
+        }
+        double averageScore = score / theaterCount;
         return (averageScore / 100) * 3;
     }
 }
